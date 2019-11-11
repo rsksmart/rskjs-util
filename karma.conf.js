7f17196d3e47cf56e718761b9f70fe3db899d8c9
emi <emiliorizzo@gmail.com>
2019-11-11 15:36:25 +0000

    Update dependencies and change file structure
    - Update babel
    - Move index.js to src/addresses.js
    - Update karma & karma.config

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,70 @@
 module.exports = function (config) {
   config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['browserify', 'detectBrowsers', 'mocha'],
+
+
+    // list of files / patterns to load in the browser
     files: [
-      'test/*.js'
+      'test/*.spec.js'
     ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.js': ['browserify', 'env']
+      'test/*.spec.js': ['browserify', 'env']
     },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-    plugins: [
-      'karma-browserify',
-      'karma-chrome-launcher',
-      'karma-env-preprocessor',
-      'karma-firefox-launcher',
-      'karma-detect-browsers',
-      'karma-mocha'
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+    envPreprocessor: [
+      'RANDOM_TESTS_REPEAT',
+      'TRAVIS'
     ],
     browserify: {
       'transform': [
@@ -22,17 +72,14 @@ module.exports = function (config) {
           'babelify',
           {
             'presets': [
-              'env'
+              '@babel/preset-env'
             ]
           }
         ]
       ],
+      extensions: ['.js'],
       debug: true
     },
-    envPreprocessor: [
-      'RANDOM_TESTS_REPEAT',
-      'TRAVIS'
-    ],
     detectBrowsers: {
       enabled: true,
       usePhantomJS: false,
@@ -41,7 +88,7 @@ module.exports = function (config) {
           return ['Firefox']
         }
 
-        var browsers = ['Chrome', 'Firefox']
+        const browsers = ['Chrome', 'Firefox']
         return browsers.filter(function (browser) {
           return availableBrowser.indexOf(browser) !== -1
         })
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const addresses = require('./addresses')
+const modules = Object.assign({}, addresses)
+module.exports = Object.freeze(modules)
